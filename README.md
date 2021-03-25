82d583dbf8e98b95c6f0a6ab81de24a72416d635
trung <>
2021-03-25 22:25:13 +0700

    1. Use pragma solidity ^0.6.12; for all contracts 2. Compile/deploy contracts to local ganache 3. README

--- README.md
+++ README.md
@@ -1,12 +1,24 @@
-# Smart contracts for Voveo assemblies
+# assembly-contracts
+Smart contracts for running assemblies on Voveo
 
-These are the solidity contracts for assemblies and voting of [Voveo](https://voveo.ch).
+## Installation
 
-## Get started
+`npm i`
 
-1. clone repo
-1. run `truffle compile --all` or ` npx truffle compile --all --config truffle-config.js` (dry run)
-1. run `truffle migrate --network NETWORK` or ` npx migrate --network NETWORK --config truffle-config.js`
-1. NETWORK is the code for the blockchain as configure in truffle-config, e.g. goerli or main
-1. with this command, the library contracts are deployed on the blockchain.
-1. run `truffle compile --all` again in order to get the correct bytecode (now linked to the library contract)
+## Compile contracts
+
+`npm run build`
+
+## Deploy contract
+
+### Deploy contract to local Ganache
+
+1. Start local ganache
+
+`npm run ganache`
+
+2. Deploy
+
+`npm run migrate-local`
+
+
--- truffle-config.js
+++ truffle-config.js
@@ -1,51 +1,171 @@
-module.exports = {
-  // Uncommenting the defaults below
-  // provides for an easier quick-start with Ganache.
-  // You can also follow this format for other networks;
-  // see <http://truffleframework.com/docs/advanced/configuration>
-  // for more details on how to specify configuration options!
-  //
-  //networks: {
-  //  development: {
-  //    host: "127.0.0.1",
-  //    port: 7545,
-  //    network_id: "*"
-  //  },
-  //  test: {
-  //    host: "127.0.0.1",
-  //    port: 7545,
-  //    network_id: "*"
-  //  }
-  //}
+/**
+ * Use this file to configure your truffle project. It's seeded with some
+ * common settings for different networks and features like migrations,
+ * compilation and testing. Uncomment the ones you need or modify
+ * them to suit your project as necessary.
+ *
+ * More information about configuration can be found at:
+ *
+ * truffleframework.com/docs/advanced/configuration
+ *
+ * To deploy via Infura you'll need a wallet provider (like truffle-hdwallet-provider)
+ * to sign your transactions before they're sent to a remote public node. Infura API
+ * keys are available for free at: infura.io/register
+ *
+ * You'll also need a mnemonic - the twelve word phrase the wallet uses to generate
+ * public/private key pairs. If you're publishing your code to GitHub make sure you load this
+ * phrase from a file you've .gitignored so it doesn't accidentally become public.
+ *
+ */
 
-  compilers: {
-    solc: {
-      version: "0.6.12",
-      settings: {
-        optimizer: {
-          enabled: true,
-          //runs: 5
-        },
-        libraries: {
-          "contracts/Assembly.sol": {
-            LibAssembly: "0x8790A9166E24ea0212F69843Af1cc139c8Cf7849", // goerli: "0x137db383A731C278555C458955EEa9431d07769a",
-            libsign: "0x34349d057D2a42056f11eaAF8Cb6890F0B71e512", // goerli: "0xc422a2cE13f5Bc04Ca2F36af4Fe12a63232E6e31",
-          },
-          "contracts/LibAssembly.sol": {
-            libsign: "0x34349d057D2a42056f11eaAF8Cb6890F0B71e512", // goerli: "0xc422a2cE13f5Bc04Ca2F36af4Fe12a63232E6e31",
-          },
-          "contracts/signed.sol": {
-            libsign: "0x34349d057D2a42056f11eaAF8Cb6890F0B71e512", // goerli: "0xc422a2cE13f5Bc04Ca2F36af4Fe12a63232E6e31",
-          },
-          "contracts/LibVoting.sol": {
-            libsign: "0x34349d057D2a42056f11eaAF8Cb6890F0B71e512", // goerli: "0xc422a2cE13f5Bc04Ca2F36af4Fe12a63232E6e31",
-          },
-          "contracts/Voting.sol": {
-            LibVoting: "0xB32e98b87e03eCd81C47180A0051d9eb6609F060", // goerli: "0x1Fba0a861879b63bFEf0c59088945b8053f20267",
-            libsign: "0x34349d057D2a42056f11eaAF8Cb6890F0B71e512", // goerli: "0xc422a2cE13f5Bc04Ca2F36af4Fe12a63232E6e31",
-          },
-        },
-      },
-    },
-  },
-};
+ const HDWalletProvider = require("@truffle/hdwallet-provider");
+ const infuraKey = "v3/1c60b584fddc49deb2efaa247257cdd2";
+ 
+ const fs = require("fs");
+ //const mnemonic = fs.readFileSync(".secret").toString().trim();
+ const mnemonic = "";
+ const privatekey = fs.readFileSync(process.env.SECRET_FILE || ".secret.ganache").toString();
+ const teamsMnemonic = "favorite split royal cover bus spray pitch alley grocery fantasy firm liberty";
+ 
+ module.exports = {
+   plugins: ["truffle-security"],
+ 
+   contracts_directory: "contracts",
+ 
+   /**
+    * Networks define how you connect to your ethereum client and let you set the
+    * defaults web3 uses to send transactions. If you don't specify one truffle
+    * will spin up a development blockchain for you on port 9545 when you
+    * run `develop` or `test`. You can ask a truffle command to use a specific
+    * network from the command line, e.g
+    *
+    * $ truffle test --network <network-name>
+    */
+ 
+   networks: {
+     // Useful for testing. The `development` name is special - truffle uses it by default
+     // if it's defined here and no other network is specified at the command line.
+     // You should run a client (like ganache-cli, geth or parity) in a separate terminal
+     // tab if you use this network and you must also set the `host`, `port` and `network_id`
+     // options below to some value.
+     //
+     // Another network with more advanced options...
+     // advanced: {
+     // port: 8777,             // Custom port
+     // network_id: 1342,       // Custom network
+     // gas: 8500000,           // Gas sent with each transaction (default: ~6700000)
+     // gasPrice: 20000000000,  // 20 gwei (in wei) (default: 100 gwei)
+     // from: <address>,        // Account to send txs from (default: accounts[0])
+     // websockets: true        // Enable EventEmitter interface for web3 (default: false)
+     // },
+     // Useful for deploying to a public network.
+     // NB: It's important to wrap the provider as a function.
+     ropsten: {
+       provider: () => new HDWalletProvider(mnemonic, `https://ropsten.infura.io/${infuraKey}`),
+       network_id: 3, // Ropsten's id
+       gas: 5500000, // Ropsten has a lower block limit than mainnet
+       confirmations: 2, // # of confs to wait between deployments. (default: 0)
+       timeoutBlocks: 200, // # of blocks before a deployment times out  (minimum/default: 50)
+       skipDryRun: true, // Skip dry run before migrations? (default: false for public nets )
+     },
+     goerli: {
+       provider: () => new HDWalletProvider(privatekey, `https://goerli.infura.io/v3/62db62ec8ca146d29038a146ee09ae0e`),
+       network_id: 5,
+       gas: 5500000,
+       confirmations: 2,
+       timeoutBlocks: 200,
+       skipDryRun: true,
+     },
+     // Useful for private networks
+     // private: {
+     // provider: () => new HDWalletProvider(mnemonic, `https://network.io`),
+     // network_id: 2111,   // This network is yours, in the cloud.
+     // production: true    // Treats this network as if it was a public net. (default: false)
+     // }
+     development: {
+       host: "127.0.0.1",
+       port: 8545,
+       network_id: "*",
+     },
+     test: {
+       host: "127.0.0.1",
+       port: 8546,
+       network_id: "*",
+     },
+     teams: {
+       provider: function () {
+         return new HDWalletProvider(teamsMnemonic, "https://sandbox.truffleteams.com/01b581c6-0ffe-46ce-9dc7-d4d76823077b", 0, 10, false);
+       },
+       network_id: 1605810390116,
+     },
+   },
+ 
+   // Set default mocha options here, use special reporters etc.
+   mocha: {
+     // timeout: 100000
+   },
+ 
+   // Configure your compilers
+   compilers: {
+     solc: {
+       version: "0.6.12",
+       parser: "solcjs",
+       // version: "0.5.1",    // Fetch exact version from solc-bin (default: truffle's version)
+       // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
+       /*
+       outputSelection: {
+         "*": {
+           "*": ["metadata", "abi", "evm.bytecode", "evm.bytecode.linkReferences", "evm.gasEstimates"]
+         }
+       },
+       */
+       settings: {
+         optimizer: {
+           enabled: true,
+           runs: 200
+         },
+        //  libraries: {
+        //    "contracts/Assembly.sol": {
+        //      LibAssembly: "0x25E771988BCf8F773d2846C08EAE99865D3f1aed",
+        //      // simons local ganache: "0xaD888d0Ade988EbEe74B8D4F39BF29a8d0fe8A8D",
+        //      // teams: "0x25E771988BCf8F773d2846C08EAE99865D3f1aed",
+        //      // goerli: "0x137db383A731C278555C458955EEa9431d07769a",
+        //      libsign: "0x8E9AFDC0AE930873Acad7DC2C704d3771E68f692",
+        //      // simons local ganache:"0xD86C8F0327494034F60e25074420BcCF560D5610",
+        //      // teams: "0x8E9AFDC0AE930873Acad7DC2C704d3771E68f692",
+        //      // goerli: "0xc422a2cE13f5Bc04Ca2F36af4Fe12a63232E6e31",
+        //    },
+        //    "contracts/LibAssembly.sol": {
+        //      libsign: "0x8E9AFDC0AE930873Acad7DC2C704d3771E68f692",
+        //      // simons local ganache:"0xD86C8F0327494034F60e25074420BcCF560D5610",
+        //      // teams: "0x8E9AFDC0AE930873Acad7DC2C704d3771E68f692",
+        //      // goerli: "0xc422a2cE13f5Bc04Ca2F36af4Fe12a63232E6e31",
+        //    },
+        //    "contracts/signed.sol": {
+        //      libsign: "0x8E9AFDC0AE930873Acad7DC2C704d3771E68f692",
+        //      // simons local ganache:"0xD86C8F0327494034F60e25074420BcCF560D5610",
+        //      // teams: "0x8E9AFDC0AE930873Acad7DC2C704d3771E68f692",
+        //      // goerli: "0xc422a2cE13f5Bc04Ca2F36af4Fe12a63232E6e31",
+        //    },
+        //    "contracts/LibVoting.sol": {
+        //      libsign: "0x8E9AFDC0AE930873Acad7DC2C704d3771E68f692",
+        //      // simons local ganache: "0xD86C8F0327494034F60e25074420BcCF560D5610",
+        //      // teams: "0x8E9AFDC0AE930873Acad7DC2C704d3771E68f692",
+        //      // goerli: "0xc422a2cE13f5Bc04Ca2F36af4Fe12a63232E6e31",
+        //    },
+        //    "contracts/Voting.sol": {
+        //      LibVoting: "0x1351C98F794c7D1EACFb5276dA70A1cACB9e88B4",
+        //      // simons local ganache:"0x4bf749ec68270027C5910220CEAB30Cc284c7BA2",
+        //      // teams: "0x1351C98F794c7D1EACFb5276dA70A1cACB9e88B4",
+        //      // goerli: "0x1Fba0a861879b63bFEf0c59088945b8053f20267",
+        //      libsign: "0x8E9AFDC0AE930873Acad7DC2C704d3771E68f692",
+        //      // simons local ganache:"0xD86C8F0327494034F60e25074420BcCF560D5610",
+        //      // teams: "0x8E9AFDC0AE930873Acad7DC2C704d3771E68f692",
+        //      // goerli: "0xc422a2cE13f5Bc04Ca2F36af4Fe12a63232E6e31",
+        //    },
+        //  },
+       },
+     },
+   },
+ };
+ 
